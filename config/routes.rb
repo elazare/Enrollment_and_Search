45bd2051658bdedc23a79e95b36e24a73a24984f
Eli Esrig <elazare@brandeis.edu>
2018-10-22 20:59:23 -0400

    Working on some display stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/signup', to: 'users#new'
   resources :coursubs
   resources :courses
   resources :subjects
   resources :instructors
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
