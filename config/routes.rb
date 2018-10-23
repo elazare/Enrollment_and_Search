19ba8d6f6a7165790b131367aa9dedd2497564dd
Eli Esrig <elazare@brandeis.edu>
2018-10-23 03:33:03 -0400

    Added Gravatar support

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/signup', to: 'users#new'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
   resources :coursubs
   resources :courses
   resources :subjects
