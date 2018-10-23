d5b6fefa2afbfbca31d42bc2a3fcc2afda27c186
Eli Esrig <elazare@brandeis.edu>
2018-10-23 09:30:52 -0400

    Added the beginning of session support

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get '/signup', to: 'users#new'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   post '/signup',  to: 'users#create'
-  resources :coursubs
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :courses
   resources :subjects
   resources :instructors
